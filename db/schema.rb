3e4bf22a9a23a21070756ad83a99b84ee9082694
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-01-18 15:43:54 -0800

    added name attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113203621) do
+ActiveRecord::Schema.define(version: 20170118233718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,9 @@ ActiveRecord::Schema.define(version: 20170113203621) do
     t.datetime "updated_at",                             null: false
     t.boolean  "instructor",             default: true
     t.boolean  "student",                default: false
+    t.string   "auth_token",             default: ""
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
