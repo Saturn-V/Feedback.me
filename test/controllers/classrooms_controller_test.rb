8fd4c47881d39099a0ddae562fbca2de1f92ed35
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-01-23 10:46:14 -0800

    started controller tests

--- test/controllers/classrooms_controller_test.rb
+++ test/controllers/classrooms_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ClassroomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = User.create(email: 'adam@instructor.com', password: 'password', password_confirmation: 'password', first_name: 'Adam', last_name: 'Braus', instructor: true, student: false)
+    @user.save
+    sign_in @user
+    @classroom = @user.classrooms.create(name: 'Ruby on Rails', class_code: 'xyz', subject: 'CS')
+  end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "should get index" do
+    get classrooms_url
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get classroom_url(@classroom)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_classroom_url
+    assert_response :success
+  end
+
+  test "should create classroom" do
+    assert_difference('Classroom.count') do
+      post classrooms_url, params: { classroom: { name: 'Ruby on Rails', class_code: 'xyz', subject: 'CS' } }
+    end
+  end
 end
--- test/controllers/responses_controller_test.rb
+++ test/controllers/responses_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ResponsesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = User.create(email: 'adam@instructor.com', password: 'password', password_confirmation: 'password', first_name: 'Adam', last_name: 'Braus', instructor: true, student: false)
+    @user.save
+    sign_in @user
+    @classroom = @user.classrooms.create(name: 'Ruby on Rails', class_code: 'xyz', subject: 'CS')
+  end
+
+  test "the truth" do
+    assert true
+  end
+
+  test 'should get edit' do
+    get edit_subreddit_url(@subreddit)
+  end
+
+  test 'should update subreddit' do
+    patch subreddit_url(@subreddit), params: { subreddit: { title: 'updated' } }
+
+    # take user to 'show' subreddit after editing one
+    assert_redirected_to subreddit_path(@subreddit)
+
+    # Reload data to fetch updated data and assert that title is updated.
+    @subreddit.reload
+
+    assert_equal 'updated', @subreddit.title
+  end
 end
