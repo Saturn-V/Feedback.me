3fac5b40c29f66efd1eaeec7b3053843d677c399
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-02-14 23:02:59 -0800

    [UPDATE][UI][UX] HOOOOT STUFF m8, redesign to go along with app logo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
   def home
     @classrooms = current_user.classrooms
-    @feedback_requests = current_user.feedback_requests
+    @feedback_requests = current_user.feedback_requests.last(3)
   end
 
   def landing
